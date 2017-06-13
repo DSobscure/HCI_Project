62cd9dd4c77163f28fb2489c3d0b3c378b664c43
Alen Works <fcs60212@hotmail.com.tw>
2017-06-13 22:27:27 +0800

    floor update

--- HCI_Project.Unity/Assets/Scripts/Gamer.cs
+++ HCI_Project.Unity/Assets/Scripts/Gamer.cs
@@ -10,7 +10,7 @@ public class Gamer : MonoBehaviour
 	void Update ()
     {
         var arTransform = m_camera.transform;
-        transform.position = new Vector3(arTransform.position.x, transform.position.y, arTransform.position.z);
-        transform.rotation = arTransform.rotation;//Quaternion.Euler(0, arTransform.rotation.eulerAngles.y, arTransform.rotation.eulerAngles.z);
+        transform.position = new Vector3(arTransform.position.x, arTransform.position.y, arTransform.position.z);
+        //transform.rotation = arTransform.rotation;//Quaternion.Euler(0, arTransform.rotation.eulerAngles.y, arTransform.rotation.eulerAngles.z);
     }
 }
