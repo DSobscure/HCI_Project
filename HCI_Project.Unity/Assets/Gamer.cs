d5482064e931f768706716e94f6bf3d720a86c09
Alen Works <fcs60212@hotmail.com.tw>
2017-06-13 15:52:22 +0800

    0613

---
+++ HCI_Project.Unity/Assets/Gamer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamer : MonoBehaviour {
+
+    public Camera m_camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = m_camera.transform.position;
+	}
+}
